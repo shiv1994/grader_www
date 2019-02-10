d49fec86ca2d7d32a8c7a3bec73602586a35b045
Shiva Ramoudith <shivaramoudith@Shivas-MacBook-Pro.local>
2019-02-10 08:52:00 -0400

    Removed ; again and merged requirements.

--- src/validator.py
+++ src/validator.py
@@ -7,7 +7,7 @@ def validateProject():
     report = {}
     for file in os.listdir(path):
         if file.endswith(".html"):
-            file = os.path.join(path, file);
+            file = os.path.join(path, file)
             report[file] = validate(file)
     return report
         
