c66d5a7a6215db04e5769f73cbb9a74d37d767de
Shiva Ramoudith <shivaramoudith@Shivas-MacBook-Pro.local>
2019-02-10 08:49:38 -0400

    Removed ; from code.

--- src/webvalidator.py
+++ src/webvalidator.py
@@ -7,8 +7,8 @@ def validateProject():
     report = {}
     for file in os.listdir(path):
         if file.endswith(".html"):
-            file = os.path.join(path, file);
+            file = os.path.join(path, file)
             report[file] = requests.post('https://validator.w3.org/nu/?out=json', files={'upload_file': open(file, 'rb')})
-    return report;
+    return report
         
     
